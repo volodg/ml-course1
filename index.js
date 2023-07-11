900e68bc9c78acb0ad55b0c9c1d22d0d1e4c89f6
volodg <vova.gorbenko@gmail.com>
2023-07-11 09:34:32 +0200

    add canvas

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-import { greet } from './pkg';
-
-greet('World');
+// For more comments about what's going on here, check out the `hello_world`
+// example.
+import('./pkg')
+    .catch(console.error);
