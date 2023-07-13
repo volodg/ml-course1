df9ba5976c481eae2dc3e28b828e9f12b4ea350c
volodg <vova.gorbenko@gmail.com>
2023-07-13 16:32:51 +0200

    add viewer

---
+++ viewer/src/lib.rs
@@ -0,0 +1,15 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+fn start() -> Result<(), JsValue> {
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
