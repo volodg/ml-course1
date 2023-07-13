5713e01c2f698065253aec05bed32cd905cadda3
volodg <vova.gorbenko@gmail.com>
2023-07-13 10:24:25 +0200

    implementing data set tool

--- data_set/src/main.rs
+++ data_set/src/main.rs
@@ -1,10 +1,22 @@
+use std::path::PathBuf;
 use const_format::concatcp;
 
 const DATA_DIR: &str = "./data";
-const SET_DIR: &str = "/dataset";
-const DATASET_DIR: &str = concatcp!(DATA_DIR, SET_DIR);
+const RAW_DIR: &str = concatcp!(DATA_DIR, "/raw");
+#[allow(dead_code)]
+const DATASET_DIR: &str = concatcp!(DATA_DIR, "/dataset");
+#[allow(dead_code)]
+const JSON_DIR: &str = concatcp!(DATA_DIR, "/json");
+#[allow(dead_code)]
+const IMG_DIR: &str = concatcp!(DATA_DIR, "/img");
+#[allow(dead_code)]
+const SAMPLES: &str = concatcp!(DATA_DIR, "/samples.json");
 
 fn main() -> Result<(), std::io::Error> {
-    println!("Hello, world! {}", DATASET_DIR);
+    let entries: Vec<PathBuf> = std::fs::read_dir(RAW_DIR)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<_, _>>()?;
+
+    println!("Hello, world! {:?}", entries);
     Ok(())
 }
