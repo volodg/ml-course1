b028d5dd6d6bca3a4a53803f3ab02dd43d585357
volodg <vova.gorbenko@gmail.com>
2023-07-13 10:15:25 +0200

    implementing data set tool

--- data_set/src/main.rs
+++ data_set/src/main.rs
@@ -1,4 +1,10 @@
+use const_format::concatcp;
 
-fn main() {
-    println!("Hello, world!");
+const DATA_DIR: &str = "./data";
+const SET_DIR: &str = "/dataset";
+const DATASET_DIR: &str = concatcp!(DATA_DIR, SET_DIR);
+
+fn main() -> Result<(), std::io::Error> {
+    println!("Hello, world! {}", DATASET_DIR);
+    Ok(())
 }
