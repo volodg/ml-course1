0d513738a174b147f6327285fee9e6cddf6c684d
volodg <vova.gorbenko@gmail.com>
2023-07-14 17:04:10 +0200

    add trigonometry app

---
+++ trigonometry/src/lib.rs
@@ -0,0 +1,16 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::JsValue;
+
+#[wasm_bindgen(start)]
+fn start() -> Result<(), JsValue> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
