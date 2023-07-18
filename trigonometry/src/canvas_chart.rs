5f406b4568d6dabe2063c3e4ac261b1388d9fd01
volodg <vova.gorbenko@gmail.com>
2023-07-18 13:04:44 +0200

    draw cos and tan

--- trigonometry/src/canvas_chart.rs
+++ trigonometry/src/canvas_chart.rs
@@ -23,10 +23,23 @@ impl DrawWithState for CanvasChart {
         self.canvas.draw(app_state)?;
 
         let chart_scale = self.canvas.offset[1] as f64;
+
         self.canvas.context.draw_point_with_size_and_color(
             &[((app_state.get_theta() * chart_scale).round()) as i32, (app_state.get_sin() * chart_scale) as i32],
             2,
             "red"
+        );
+
+        self.canvas.context.draw_point_with_size_and_color(
+            &[((app_state.get_theta() * chart_scale).round()) as i32, (app_state.get_cos() * chart_scale) as i32],
+            2,
+            "blue"
+        );
+
+        self.canvas.context.draw_point_with_size_and_color(
+            &[((app_state.get_theta() * chart_scale).round()) as i32, (app_state.get_tan() * chart_scale) as i32],
+            2,
+            "magenta"
         );
 
         Ok(())
