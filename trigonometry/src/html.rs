f8eb121113fd1ed937aa8c4503efed392c9a20ab
volodg <vova.gorbenko@gmail.com>
2023-07-18 12:05:16 +0200

    add chart canvas

--- trigonometry/src/html.rs
+++ trigonometry/src/html.rs
@@ -6,15 +6,17 @@ use web_sys::{window, CanvasRenderingContext2d, Document, HtmlCanvasElement};
 #[derive(Clone)]
 pub struct HtmlDom {
     pub document: Document,
-    pub context: CanvasRenderingContext2d,
     pub canvas: HtmlCanvasElement,
+    pub context: CanvasRenderingContext2d,
     pub offset: [i32; 2],
+    pub chart_canvas: HtmlCanvasElement,
+    pub chart_context: CanvasRenderingContext2d,
+    pub chart_offset: [i32; 2],
 }
 
 impl HtmlDom {
-    pub fn create() -> Result<Self, JsValue> {
-        let document = window().unwrap().document().unwrap();
-        let canvas = document.get_element_by_id("myCanvas").unwrap();
+    fn get_canvas_and_context(document: &Document, id: &str) -> Result<(HtmlCanvasElement, CanvasRenderingContext2d), JsValue> {
+        let canvas = document.get_element_by_id(id).unwrap();
         let canvas = canvas.dyn_into::<HtmlCanvasElement>()?;
 
         let context = canvas
@@ -22,15 +24,28 @@ impl HtmlDom {
             .unwrap()
             .dyn_into::<CanvasRenderingContext2d>()?;
 
-        let offset = [canvas.width() as i32 / 2, canvas.height() as i32 / 2];
+        Ok((canvas, context))
+    }
 
+    pub fn create() -> Result<Self, JsValue> {
+        let document = window().unwrap().document().unwrap();
+        let (canvas, context) = Self::get_canvas_and_context(&document, "myCanvas")?;
+        let (chart_canvas, chart_context) = Self::get_canvas_and_context(&document, "chartCanvas")?;
+
+        let offset = [canvas.width() as i32 / 2, canvas.height() as i32 / 2];
         let _ = context.translate(offset[0].into(), offset[1].into());
+
+        let chart_offset = [chart_canvas.width() as i32 / 2, chart_canvas.height() as i32 / 2];
+        let _ = chart_context.translate(chart_offset[0].into(), chart_offset[1].into());
 
         Self {
             document,
-            context,
             canvas,
+            context,
             offset,
+            chart_canvas,
+            chart_context,
+            chart_offset,
         }
         .ok()
     }
