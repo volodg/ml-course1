54c24f1ab5029bef73bafad7685334e5a4d6649b
volodg <vova.gorbenko@gmail.com>
2023-07-22 12:41:26 +0200

    cleanup

--- demo_chart/src/html.rs
+++ demo_chart/src/html.rs
@@ -1,7 +1,7 @@
 use crate::canvas::Canvas;
 use commons::utils::OkExt;
-use wasm_bindgen::JsValue;
 use wasm_bindgen::JsCast;
+use wasm_bindgen::JsValue;
 use web_sys::{window, Document, HtmlTableElement};
 
 #[derive(Clone)]
@@ -19,6 +19,11 @@ impl HtmlDom {
         let data_table = document.get_element_by_id("dataTable").unwrap();
         let data_table = data_table.dyn_into::<HtmlTableElement>()?;
 
-        Self { document, canvas, data_table }.ok()
+        Self {
+            document,
+            canvas,
+            data_table,
+        }
+        .ok()
     }
 }
