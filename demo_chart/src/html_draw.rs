4f110af58535412a8454acc5739b62565c6aa5fb
volodg <vova.gorbenko@gmail.com>
2023-07-24 20:30:24 +0200

    add sample selection

--- demo_chart/src/html_draw.rs
+++ demo_chart/src/html_draw.rs
@@ -2,14 +2,12 @@ use crate::app_state::{AppState, CarType};
 use crate::draw::DrawWithState;
 use crate::html::HtmlDom;
 use std::collections::HashMap;
+use web_commons::html::AddListener;
 use wasm_bindgen::JsCast;
 use wasm_bindgen::JsValue;
 use web_commons::chart::Chart;
 use web_commons::chart_models::{Options, Sample, SampleStyle, SampleStyleType};
-use web_sys::{
-    Element, HtmlTableRowElement, HtmlTableSectionElement, ScrollBehavior, ScrollIntoViewOptions,
-    ScrollLogicalPosition,
-};
+use web_sys::{Element, HtmlTableRowElement, HtmlTableSectionElement, ScrollBehavior, ScrollIntoViewOptions, ScrollLogicalPosition, window};
 
 fn default_chart_options() -> Options {
     let mut styles = HashMap::<String, SampleStyle>::new();
@@ -57,6 +55,12 @@ impl DrawWithState for HtmlDom {
         for sample in &app_state.samples {
             let tr = body.insert_row()?.dyn_into::<HtmlTableRowElement>()?;
             tr.set_id(sample.element_id().as_str());
+            {
+                let sample = sample.clone();
+                tr.on_click(move |_| {
+                    handle_click(&sample).expect("");
+                })?;
+            }
             tr.insert_cell()?
                 .set_inner_html(sample.id.to_string().as_str());
             tr.insert_cell()?.set_inner_html(sample.label.as_str());
@@ -66,23 +70,8 @@ impl DrawWithState for HtmlDom {
                 .set_inner_html(std::format!("{:.0}", sample.point.y).as_str());
         }
 
-        let document = self.document.clone();
         let on_click_callback = move |sample: &Sample| {
-            let selected = document.query_selector_all(".emphasize").expect("");
-            for i in 0..selected.length() {
-                let element = selected.item(i).expect("").dyn_into::<Element>().expect("");
-                element.class_list().remove_1("emphasize").expect("");
-            }
-
-            let element = document
-                .get_element_by_id(sample.element_id().as_str())
-                .expect("");
-            element.class_list().add_1("emphasize").expect("");
-
-            let mut options = ScrollIntoViewOptions::new();
-            options.behavior(ScrollBehavior::Auto);
-            options.block(ScrollLogicalPosition::Center);
-            element.scroll_into_view_with_scroll_into_view_options(&options);
+            handle_click(sample).expect("");
         };
 
         let chart = Chart::create(
@@ -94,6 +83,25 @@ impl DrawWithState for HtmlDom {
         let borrow_chart = chart.borrow();
         borrow_chart.draw()
     }
+}
+
+fn handle_click(sample: &Sample) -> Result<(), JsValue> {
+    let document = window().expect("").document().expect("");
+    let selected = document.query_selector_all(".emphasize")?;
+    for i in 0..selected.length() {
+        let element = selected.item(i).expect("").dyn_into::<Element>()?;
+        element.class_list().remove_1("emphasize")?;
+    }
+
+    let element = document.get_element_by_id(sample.element_id().as_str()).expect("");
+    element.class_list().add_1("emphasize")?;
+
+    let mut options = ScrollIntoViewOptions::new();
+    options.behavior(ScrollBehavior::Auto);
+    options.block(ScrollLogicalPosition::Center);
+    element.scroll_into_view_with_scroll_into_view_options(&options);
+
+    Ok(())
 }
 
 trait SampleExt {
