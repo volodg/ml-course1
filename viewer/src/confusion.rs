d2c18ae5bde60734197d343ff990d46a94a74d84
volodg <vova.gorbenko@gmail.com>
2023-07-31 01:50:17 +0200

    cleanup

--- viewer/src/confusion.rs
+++ viewer/src/confusion.rs
@@ -118,8 +118,8 @@ impl Confusion {
         Ok(())
     }
 
-    fn prepare_matrix(&self, cells_row_count: usize) -> Vec<Vec<i64>> {
-        let mut result = Vec::with_capacity(cells_row_count);
+    fn prepare_matrix(&self, cells_row_count: usize) -> Vec<Vec<String>> {
+        let mut matrix = Vec::with_capacity(cells_row_count);
 
         for _ in 0..cells_row_count {
             let mut row = Vec::with_capacity(cells_row_count);
@@ -128,7 +128,7 @@ impl Confusion {
                 row.push(0)
             }
 
-            result.push(row)
+            matrix.push(row)
         }
 
         for sample in &self.samples {
@@ -142,18 +142,39 @@ impl Confusion {
                 .iter()
                 .position(|x| sample.label == *x)
                 .expect("");
-            result[row + 1][column + 1] += 1;
+            matrix[row + 1][column + 1] += 1;
         }
 
         for i in 1..cells_row_count {
             for j in 1..cells_row_count {
-                result[0][j] += result[i][j];
-                result[i][0] += result[i][j];
+                matrix[0][j] += matrix[i][j];
+                matrix[i][0] += matrix[i][j];
             }
         }
 
         for i in 1..cells_row_count {
-            result[0][i] -= result[i][0];
+            matrix[0][i] -= matrix[i][0];
+        }
+
+        // Build string representation
+        let mut result = vec![];
+
+        for i in 0..cells_row_count {
+            let mut row = vec![];
+
+            for j in 0..cells_row_count {
+                let value = matrix[i][j];
+                let value = if i == 0 && j == 0 {
+                    "".to_string()
+                } else if value > 0 && i == 0 {
+                    std::format!("+{value}")
+                } else {
+                    matrix[i][j].to_string()
+                };
+                row.push(value)
+            }
+
+            result.push(row)
         }
 
         result
@@ -164,7 +185,7 @@ impl Confusion {
         table: HtmlElement,
         cells_row_count: usize,
         cell_size: f64,
-        matrix: Vec<Vec<i64>>,
+        matrix: Vec<Vec<String>>,
     ) -> Result<(), JsValue> {
         for i in 0..cells_row_count {
             let row = self.document.create_element("tr")?;
@@ -181,7 +202,7 @@ impl Confusion {
                     .set_property("height", std::format!("{cell_size}px").as_str())?;
                 cell.style().set_property("padding", "0")?;
 
-                cell.set_text_content(Some(matrix[i][j].to_string().as_str()));
+                cell.set_text_content(Some(matrix[i][j].as_str()));
 
                 row.append_child(&cell)?;
             }
