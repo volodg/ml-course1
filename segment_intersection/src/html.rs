8a8108c1338984777689dbde20282e851718438b
volodg <vova.gorbenko@gmail.com>
2023-08-05 21:39:20 +0200

    draw two lines

--- segment_intersection/src/html.rs
+++ segment_intersection/src/html.rs
@@ -1,20 +1,34 @@
 use commons::utils::OkExt;
 use wasm_bindgen::JsCast;
 use wasm_bindgen::JsValue;
-use web_sys::{window, Document, HtmlCanvasElement};
+use web_sys::{window, CanvasRenderingContext2d, Document, HtmlCanvasElement, Window};
 
 pub struct HtmlDom {
+    pub window: Window,
     pub document: Document,
     pub canvas: HtmlCanvasElement,
+    pub context: CanvasRenderingContext2d,
 }
 
 impl HtmlDom {
     pub fn create() -> Result<Self, JsValue> {
-        let document = window().unwrap().document().unwrap();
+        let window = window().unwrap();
+        let document = window.document().unwrap();
 
-        let canvas = document.get_element_by_id("canvas").unwrap();
+        let canvas = document.get_element_by_id("myCanvas").unwrap();
         let canvas = canvas.dyn_into::<HtmlCanvasElement>()?;
 
-        Self { document, canvas }.ok()
+        let context = canvas
+            .get_context("2d")?
+            .unwrap()
+            .dyn_into::<CanvasRenderingContext2d>()?;
+
+        Self {
+            window,
+            document,
+            canvas,
+            context,
+        }
+        .ok()
     }
 }
--- segment_intersection/src/html_draw.rs
+++ segment_intersection/src/html_draw.rs
@@ -4,16 +4,27 @@ use crate::html::HtmlDom;
 use std::cell::RefCell;
 use std::rc::Rc;
 use wasm_bindgen::JsValue;
+use commons::geometry::{Point2D, Point2DView};
 use web_commons::log;
 
 impl DrawWithState for HtmlDom {
     fn draw(&self, _app_state: &Rc<RefCell<AppState>>) -> Result<(), JsValue> {
         log("draw me");
+        self.canvas.set_width(self.window.inner_width().expect("").as_f64().unwrap() as u32);
+        self.canvas.set_height(self.window.inner_height().expect("").as_f64().unwrap() as u32);
 
-        let size = 300;
+        let a = Point2D::create(200.0, 150.0);
+        let b = Point2D::create(150.0, 250.0);
+        let c = Point2D::create(50.0, 100.0);
+        let d = Point2D::create(250.0, 200.0);
 
-        self.canvas.set_width(size);
-        self.canvas.set_height(size);
+        self.context.begin_path();
+        self.context.move_to(a.x, a.y);
+        self.context.line_to(b.x, b.y);
+
+        self.context.move_to(c.x, c.y);
+        self.context.line_to(d.x, d.y);
+        self.context.stroke();
 
         Ok(())
     }
